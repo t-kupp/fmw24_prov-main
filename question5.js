c2ac1914ebae13eea2f9b20d053b57a7e5d4aac1
t-kupp <kupper.thorge@gmail.com>
2024-11-26 09:50:06 +0100

    q5 done

--- question5.js
+++ question5.js
@@ -3,3 +3,27 @@
 // För varje post ska title och body placeras under varandra.
 // I desktop-vy ska posts visas i 3 kolumner.
 // I mobil-vy ska posts visas i en kolumn med 100% width.
+
+const postsList = document.querySelector("#posts-list");
+
+async function displayData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  console.log(data);
+
+  data.forEach((post) => {
+    const container = document.createElement("div");
+
+    const title = document.createElement("h4");
+    title.innerText = post.title;
+    container.appendChild(title);
+
+    const body = document.createElement("p");
+    body.innerText = post.body;
+    container.appendChild(body);
+
+    postsList.appendChild(container);
+  });
+}
+
+displayData();
